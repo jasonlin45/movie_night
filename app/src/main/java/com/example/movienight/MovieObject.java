2b9a48fb7718b2349d294cb00c57b8670827c54e
caharris02 <caharris02@email.wm.edu>
2020-09-17 12:42:47 -0400

    MovieObject Shell created: - Needs More practical implementation - Needs Constructor from JSONObject - Needs Testing - Has basic get set methods - Has Class variables
    User Object Started:
    - Has some class variables
    - Needs Assignment Methods
    - Needs Info on construction to guide constructor
    - Needs Ratio Calculating Methods

--- app/src/main/java/com/example/movienight/MovieObject.java
+++ app/src/main/java/com/example/movienight/MovieObject.java
@@ -1,4 +1,87 @@
 package com.example.movienight;
 
+
+import org.json.JSONObject;
+
+/*The Movie Object is the object that is stores information
+* retrieved from the api in its most basic forms.*/
 public class MovieObject extends Object{
+
+    //Class variables
+    private double movieID;
+    private double[] genreIDs;
+    private boolean adult;
+    private String movieTitle;
+    private double moviePopularity;
+    private JSONObject movieDat;
+
+    //Empty constructor
+    public void MovieObject(){
+    }
+
+    //placeholder constructor
+    public void MovieObject(JSONObject movieData){
+        //Turn Json to string
+        String unpJason = movieData.toString();
+
+        //TODO: Parse Jason for private variables
+        //this.movieID = 1234; //Get the movie id from the JSON
+    }
+
+    //private double movieID;
+    //setID
+    private void setMovieID(double movID){
+        this.movieID = movID;
+    }
+
+    //getId
+    protected double getMovieID(){
+        return this.movieID;
+    }
+
+
+    //    private double genreIDs[];
+    private void setGenreID(double[] genID){
+        this.genreIDs = genID;
+    }
+
+    protected double[] getGenreIDs(){
+        return this.genreIDs;
+    }
+
+
+    //    private boolean adult;
+    //set
+    private void setAdult(boolean adultRating){
+        this.adult = adultRating;
+    }
+
+    //get
+    protected boolean getAdult(){
+        return this.adult;
+    }
+
+
+    //    private String movieTitle;
+    //set
+    private void setMovieTitle(String Title){
+        this.movieTitle = Title;
+    }
+
+    //get
+    protected String getMovieTitle(){
+        return this.movieTitle;
+    }
+
+
+    //    private double moviePopularity;
+    //set
+    private void setMoviePopularity(double Score){
+        this.moviePopularity = Score;
+    }
+
+    //get
+    protected double getMoviePopularity(){
+        return this.moviePopularity;
+    }
 }
--- app/src/main/java/com/example/movienight/UserObject.java
+++ app/src/main/java/com/example/movienight/UserObject.java
@@ -1,5 +1,27 @@
 package com.example.movienight;
 
+import java.util.Dictionary;
+
 public class UserObject extends Object {
+
+    //Class Variables
+    private Dictionary genreValues;
+    private Dictionary actorValues;
+    private String[] watchListTitles;
+    private int watchListIDs;
+
+    private String userName;
+
+
+    //public void UserObject(){}
+    public void UserObject(String UN){
+        //Empty
+        this.userName = UN;
+    }
+
+    //Assignment Methods
+
+
+    //Ratio Methods
 
 }
