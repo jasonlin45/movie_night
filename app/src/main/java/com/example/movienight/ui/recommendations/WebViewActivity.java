c7563d07ec0a37156a40beded8a53c505cf0e1fc
jasonlin45 <jasonlin45@outlook.com>
2020-10-01 01:27:43 -0400

    Added share functionality for movies

--- app/src/main/java/com/example/movienight/ui/recommendations/WebViewActivity.java
+++ app/src/main/java/com/example/movienight/ui/recommendations/WebViewActivity.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.example.movienight.R;
 
@@ -21,7 +23,21 @@ public class WebViewActivity extends Activity {
         WebView wv = (WebView) findViewById(R.id.pop_webview);
         wv.setWebViewClient(new WebViewClient());
         Bundle bd = getIntent().getExtras();
-        String url = bd.getString(Intent.EXTRA_TEXT);
+        final String url = bd.getString(Intent.EXTRA_TEXT);
         wv.loadUrl(url);
+
+        Button share = findViewById(R.id.shareButton);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,
+                        "Hey, I just got recommended this movie: " + url + ", check it out!");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
     }
 }
